b90d42be2db44f2635d9d8d2f2d4d69905f0fb3c
anukul76 <anukulthebest@gmail.com>
2023-05-19 00:28:30 +0530

    atlast

--- pythoncsvmanipulation/ans.py
+++ pythoncsvmanipulation/ans.py
@@ -1,23 +1,20 @@
 import pandas as pd
 import numpy as np
+
 # Import departments.csv into a pandas DataFrame
-departments_df = pd.read_csv('ASDE Assignment - Departments (1).csv')
+departments_df= pd.read_csv('ASDE Assignment - Departments (1).csv')
 
-
+# Import employees.csv into a pandas DataFrame
 employees_df = pd.read_csv('ASDE Assignment - Employees (1).csv')
 
-
+# Import salaries.csv into a pandas DataFrame
 salaries_df = pd.read_csv('ASDE Assignment - Salaries (1).csv')
-
-#don't know the join and group by part in pandas
-
-
-orted_df = grouped_df.sort_values('amt_usd', ascending=False)
-
-
-
-# Select the top 3 rows
+join_df = departments_df.merge(employees_df, left_on='ID', right_on='DEPT ID', suffixes=('_dept', '_emp'))
+joined_df = join_df.merge(salaries_df, left_on='ID_emp', right_on='EMP_ID', suffixes=('_emp', '_sal'))
+grouped_df = joined_df.groupby(['NAME_dept']).agg({'AMT (USD)': 'mean'})
+sorted_df = grouped_df.sort_values('AMT (USD)', ascending=False)
 top_3_departments = sorted_df.head(3)
+
 
 #rename the column
 top_3_departments = top_3_departments.rename(columns={'name': 'DEPT_NAME', 'amt_usd': 'AVG_MONTH_SALARY'})
