1f034eafdc82e7ff698cb4ec11ae2fffa6271471
anukul76 <anukulthebest@gmail.com>
2023-05-18 23:06:09 +0530

    anukul

--- python troubleshooting/2nd.py
+++ python troubleshooting/2nd.py
@@ -1,7 +1,7 @@
 def compute(n):
     if n < 10:
         out = n ** 2
-    elif n < 20:
+    elif n <= 20:    #as 20 be inclusive in
         out=1
         for i in range(1, n-9):       #we have to change it as the factorial loop should start from 1, not 0.
             out *= i
